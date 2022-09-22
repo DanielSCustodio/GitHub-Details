102f77b5421f5a81550a5397e7077615c8f324cf
Daniel Custodio <play.daniel@hotmail.com>
2022-09-22 18:33:57 -0300

    feat: Routes

---
+++ github-details/src/routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './page/Home';
+import Repository from './page/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/repository/:repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
