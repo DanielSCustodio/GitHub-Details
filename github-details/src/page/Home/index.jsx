67c9d0ae04e575315964977f1aed2a90d35fb0c6
Daniel Custodio <play.daniel@hotmail.com>
2022-09-22 22:15:30 -0300

    feat: Created form

--- github-details/src/page/Home/index.jsx
+++ github-details/src/page/Home/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import Form from '../../template/Form';
+import { Container } from './styles';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Repositórios
+      </h1>
+      <Form />
+    </Container>
+  );
 }
