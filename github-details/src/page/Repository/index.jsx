fc426cd5db2e181697993ab2d1506bdc0f6625bf
Daniel Custodio <play.daniel@hotmail.com>
2022-09-24 19:21:31 -0300

    feat: Show details

--- github-details/src/page/Repository/index.jsx
+++ github-details/src/page/Repository/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import api from '../../services/api';
-import { Container } from './styles';
+import { Container, Owner, Loading, Back } from './styles';
 
 export default function Repository({ match }) {
   const [reposytory, setRepository] = React.useState({});
@@ -21,14 +21,31 @@ export default function Repository({ match }) {
           },
         }),
       ]);
-      setRepository(repoData);
-      setIssues(issuesData);
+      setRepository(repoData.data);
+      setIssues(issuesData.data);
       setLoading(false);
     }
     getData();
   }, [match.params.repositorio]);
+  console.log(issues);
 
-  return <Container></Container>;
+  if (loading) {
+    return (
+      <Loading>
+        <h3>Carregando...</h3>
+      </Loading>
+    );
+  }
+  return (
+    <Container>
+      <Back to="/">Voltar</Back>
+      <Owner>
+        <img src={reposytory.owner.avatar_url} alt={reposytory.owner.login} />
+        <h1>{reposytory.name.toUpperCase()}</h1>
+        <p>{reposytory.description}</p>
+      </Owner>
+    </Container>
+  );
 }
 
 Repository.propTypes = {
--- github-details/src/page/Repository/styles.js
+++ github-details/src/page/Repository/styles.js
@@ -1,5 +1,48 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 export const Container = styled.section`
-  background-color: red;
+  background-color: #161b22;
+  max-width: 50vw;
+  border-radius: 6px;
+  border: 1px solid #30363d;
+  margin: 0 auto;
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  width: 100%;
+
+  img {
+    width: 50px;
+    border-radius: 50%;
+  }
+
+  h1 {
+    font-size: 21px;
+    margin: 1rem 0;
+    color: #c9d1d9;
+  }
+
+  p {
+    text-align: center;
+  }
+`;
+
+export const Back = styled(Link)`
+  padding: 1rem;
+  color: #58a6ff;
 `;
